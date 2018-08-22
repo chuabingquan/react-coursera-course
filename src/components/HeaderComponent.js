6c300e8ee74bdc5aec78d50e8fb438450ac548e5
Chua Bing Quan <chuabingquan@Chuas-MacBook-Pro.local>
2018-08-22 14:21:14 +0800

    Minor refactor #2.

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -58,7 +58,7 @@ class Header extends Component {
                 </NavItem>
                 <NavItem>
                   <NavLink className="nav-link" to="/contactus">
-                    <span className="fa fa-phone fa-lg" /> Contact Us
+                    <span className="fa fa-address-card fa-lg" /> Contact Us
                   </NavLink>
                 </NavItem>
               </Nav>
